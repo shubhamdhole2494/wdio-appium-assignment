797ead065dc71105f9a05aa9f025636172cfe9d2
Shubham Dhole <sdhole@mobiquityinc.com>
2024-05-31 09:32:10 +0530

    Scenarios add for Password required. format code

--- config/wdio.android.conf.mts
+++ config/wdio.android.conf.mts
@@ -1,39 +1,43 @@
 // wdio.conf.js
-import SlackReporter from '@moroo/wdio-slack-reporter';
-import type { Options } from '@wdio/types';
+import SlackReporter from "@moroo/wdio-slack-reporter";
+import type { Options } from "@wdio/types";
 export const config: Options.Testrunner = {
+  runner: "local",
 
-  runner: 'local',
-
-  specs: [
-    '../features/myDemo.feature',
-  ],
+  specs: ["../features/myDemo.feature"],
   maxInstances: 1,
 
-  capabilities: [{
-    "appium:platformName": "Android",
-    "appium:automationName": "uiautomator2",
-    "appium:udid": "emulator-5554",
-    "appium:platformVersion": "10.0",
-    "appium:app": "./app/Android-MyDemoAppRN.1.3.0.build-244.apk",
-    "appium:waitForIdleTimeout": 3000,
-  }],
+  capabilities: [
+    {
+      "appium:platformName": "Android",
+      "appium:automationName": "uiautomator2",
+      "appium:udid": "emulator-5554",
+      "appium:platformVersion": "10.0",
+      "appium:app": "./app/Android-MyDemoAppRN.1.3.0.build-244.apk",
+      "appium:waitForIdleTimeout": 3000,
+    },
+  ],
 
-
-  logLevel: 'info',
-  services: ['appium'],
-  reporters: ['spec', ['allure', {
-    outputDir: 'allure-results',
-    disableWebdriverStepsReporting: true,
-    disableWebdriverScreenshotsReporting: false,
-    useCucumberStepReporter: true
-  }]],
+  logLevel: "info",
+  services: ["appium"],
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+        useCucumberStepReporter: true,
+      },
+    ],
+  ],
   bail: 0,
-  baseUrl: 'http://localhost',
-  path: '/',
+  baseUrl: "http://localhost",
+  path: "/",
   connectionRetryCount: 3,
   port: 4723,
-  framework: 'cucumber',
+  framework: "cucumber",
   cucumberOpts: {
     require: ["./step_definitions/*.ts"],
     timeout: 120000,
@@ -43,13 +47,7 @@ export const config: Options.Testrunner = {
     autoCompile: true,
     tsNodeOpts: {
       transpileOnly: true,
-      project: 'tsconfig.json'
+      project: "tsconfig.json",
     },
   },
-
-}
-
-
-
-
-
+};
--- step_definitions/login.steps.ts
+++ step_definitions/login.steps.ts
@@ -3,67 +3,68 @@ import { expect } from "chai";
 import testData from "../support/testdata.json" assert { type: "json" };
 import { LoginScreen } from "../screen/login.screen.ts";
 
-
 Given(/^I am on the login screen$/, async () => {
-
-    let isLoginScreenDisplayed = await LoginScreen.validateLoginScreen();
-    expect(isLoginScreenDisplayed).to.be.true
-
+  let isLoginScreenDisplayed = await LoginScreen.validateLoginScreen();
+  expect(isLoginScreenDisplayed).to.be.true;
 });
 When(/^I enter valid login credentails$/, async () => {
-
-    await LoginScreen.loginWithValidCredentials(testData.loginCreds.username, testData.loginCreds.password)
-
+  await LoginScreen.loginWithValidCredentials(
+    testData.loginCreds.username,
+    testData.loginCreds.password
+  );
 });
 Then(/^I should login successfully$/, async () => {
-
-    let homeScreenHeaderText = await LoginScreen.validateUserLoggedInSuccessfully();
-    expect(homeScreenHeaderText).to.be.equal('Products')
-    await driver.reloadSession();
+  let homeScreenHeaderText =
+    await LoginScreen.validateUserLoggedInSuccessfully();
+  expect(homeScreenHeaderText).to.be.equal("Products");
+  await driver.reloadSession();
 });
 When(/^I enter invalid username and valid password$/, async () => {
-
-    await LoginScreen.loginWithInvalidCredentials(testData.invalidUsername.username, testData.invalidUsername.password)
-
+  await LoginScreen.loginWithInvalidCredentials(
+    testData.invalidUsername.username,
+    testData.invalidUsername.password
+  );
 });
 Then(/^I should get error message$/, async () => {
-
-    let errorMsgWrongusrText = await LoginScreen.validateErrorMessage();
-    expect(errorMsgWrongusrText).to.be.equal('Provided credentials do not match any user in this service.');
-    await driver.pause(5000);
-    await driver.reloadSession();
+  let errorMsgWrongusrText = await LoginScreen.validateErrorMessage();
+  expect(errorMsgWrongusrText).to.be.equal(
+    "Provided credentials do not match any user in this service."
+  );
+  await driver.pause(5000);
+  await driver.reloadSession();
 });
 When(/^I enter lock user credentials$/, async () => {
-
-    await LoginScreen.loginWithLockCredentials(testData.lockUsername.username, testData.lockUsername.password)
-
+  await LoginScreen.loginWithLockCredentials(
+    testData.lockUsername.username,
+    testData.lockUsername.password
+  );
 });
 Then(/^I should get lock error message$/, async () => {
-
-    let errorMsgLockUsrText = await LoginScreen.validateLockUserErrorMsg();
-    expect(errorMsgLockUsrText).to.be.equal('Sorry, this user has been locked out.');
-    await driver.reloadSession();
-
+  let errorMsgLockUsrText = await LoginScreen.validateLockUserErrorMsg();
+  expect(errorMsgLockUsrText).to.be.equal(
+    "Sorry, this user has been locked out."
+  );
+  await driver.reloadSession();
 });
 When(/^I enter blank username$/, async () => {
-
-    await LoginScreen.loginWithBlankUsername(testData.blankUsername.username, testData.blankUsername.password)
-
+  await LoginScreen.loginWithBlankUsername(
+    testData.blankUsername.username,
+    testData.blankUsername.password
+  );
 });
 Then(/^I should get required username error$/, async () => {
-
-    let errorMsgBlankUsrText = await LoginScreen.validateBlankUsrMsg();
-    expect(errorMsgBlankUsrText).to.be.equal('Username is required');
-    await driver.reloadSession();
-
+  let errorMsgBlankUsrText = await LoginScreen.validateBlankUsrMsg();
+  expect(errorMsgBlankUsrText).to.be.equal("Username is required");
+  await driver.reloadSession();
 });
-
-
-
-
-
-
-
-
-
-
+When(/^I enter blank password$/, async () => {
+  await LoginScreen.loginWithBlankPassword(
+    testData.blankPassword.username,
+    testData.blankPassword.password
+  );
+});
+Then(/^I should get required password error$/, async () => {
+  let errorMsgBlankPassText = await LoginScreen.validateBlankPassMsg();
+  expect(errorMsgBlankPassText).to.be.equal("Password is required");
+  await driver.reloadSession();
+});
